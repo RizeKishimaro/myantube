47f30c329b624b0539c1af69a0617ca4fc3920fc
RizeKishimaro <jokerlove2671@gmail.com>
2024-04-20 10:51:48 +0630

    update Comments.jsx

--- src/components/videoplayer/components/Comments.jsx
+++ src/components/videoplayer/components/Comments.jsx
@@ -9,7 +9,7 @@ const Comments = ({ profilePic, text, topComment }) => {
     <div className="collapse space-y-5 space-x-4">
       <input className="min-h-0" type="checkbox" />
       <div className="collapse-title p-0 m-0 min-h-0 text-xl font-medium">
-        <div className="flex items-start space-x-4 my-2">
+        <div className="flex items-start">
           <img
             src={profilePic}
             className="w-6 h-6 rounded-full"
